777ba0cd047bae188003b8c5550a52694467e7d2
feng.hu <feng.hu@iluvatar.ai>
2019-10-08 11:01:07 +0800

    lerna test

---
+++ packages/model1/test_li.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <li>{{todo.text}}</li>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            prop:[
+                todo
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
